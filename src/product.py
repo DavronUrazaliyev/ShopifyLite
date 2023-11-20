b5eec4859a0ba4374df0e86bbb007d5d0d91fa42
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-20 09:51:54 +0500

    product

--- src/product.py
+++ src/product.py
@@ -1,1 +1,25 @@
-a = 0
+class Product:
+    """
+    class: Product
+
+    attributes:
+        1. product_id (int)
+        2. name (str)
+        3. price (float)
+        4. quantity (int)
+
+    methods:
+        1. info - information about product
+    """
+    id = 1
+
+    def __init__(self, name: str, price: float, quantity: int):
+        self.product_id = Product.id
+        self.name       = name
+        self.price      = price
+        self.quantity   = quantity
+
+        Product.id += 1
+
+    def info(self) -> str:
+        return f"{self.product_id}. {self.name} costs {self.price}. total: {self.quantity}"
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
-from main import a
-print(a)
+from src.product import Product
+
+p1 = Product(name="Iphone 12", price=450.00, quantity=67)
+p2 = Product(name="Samsung s21", price=750.00, quantity=42)
 
 
-from src.product import a
-print(a)
+print(p1.info())
+print(p2.info())
